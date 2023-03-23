89c0843bfd47258a69508841ae017f903ff788d8
jacky.inoby <sn185672@gmail.com>
2023-03-23 13:12:39 +0800

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,27 @@
+export const available = [
+  {
+    id: 1,
+    title: '如何學習程式',
+    subtitle: 'Lorem Ipsum is simply dummy',
+  },
+  {
+    id: 2,
+    title: '如何寫出item拖曳功能',
+    subtitle: 'The point of using at its layout',
+  },
+  {
+    id: 3,
+    title: '如何製作第一個API',
+    subtitle: 'Contrary to popular belief',
+  },
+  {
+    id: 4,
+    title: '資料庫如何設計',
+    subtitle: 'There are many variations',
+  },
+  {
+    id: 5,
+    title: 'UI 如何做到元件拆分',
+    subtitle: 'Etiam mollis eros eget mi.',
+  },
+];
